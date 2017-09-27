80a2e306082192d07ffc363582b37ad7bd80f9b3
Chinmay Kulkarni <chinmay@sellezely.com>
2017-09-27 21:11:32 +0530

    chat update

---
+++ public/js/chat.js
@@ -0,0 +1,32 @@
+var socket = io('http://localhost:8000/');
+
+socket.on('chat_reply', function (data) {
+    console.log(data);
+    $(".messages").append(`
+                  <li class="message left appeared">
+            <div class="avatar"></div>
+            <div class="text_wrapper">
+                <div class="text">${data.text}</div>
+            </div>
+        </li>
+    `)
+});
+
+$(document).ready(function () {
+    $('#send').click(function () {
+        var message = $(".message_input").val()
+        socket.emit("client_message", {
+            text: message
+        })
+        $(".messages").append(`
+        <li class="message right appeared">
+            <div class="avatar"></div>
+            <div class="text_wrapper">
+                <div class="text">${message}</div>
+            </div>
+        </li>
+    `)
+
+    })
+
+})
