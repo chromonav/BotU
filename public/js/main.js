686dc7eb93cfb98d5c5e9952d3965c5a54642d2d
Saurabh Narhe <saurabhnarhe123@gmail.com>
2017-10-06 13:25:22 +0530

    6

--- public/js/main.js
+++ public/js/main.js
@@ -70,3 +70,7 @@ $(".data").click(function() {
     console.log("/store-products/" + $("#sid").text());
     $.get("/store-products/" + $("#sid").text());
 });
+
+$(".input").click(function() {
+    $(".isa_success").css({ "display" : "none"});
+})
